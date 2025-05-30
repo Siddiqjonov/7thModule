921bcb46027f6311dfe530d5acbcf2a8d3bbdb5f
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-05-31 01:59:15 +0500

    packages installed and token service was finished

---
+++ ContactManager/src/ContactManager.Application/Helpers/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using ContactManager.Application.Dtos;
+using System.Security.Claims;
+
+namespace ContactManager.Application.Helpers;
+
+public interface ITokenService
+{
+    public string GenerateTokent(UserGetDto user);
+    string GenerateRefreshToken();
+    ClaimsPrincipal? GetPrincipalFromExpiredToken(string token);
+}
